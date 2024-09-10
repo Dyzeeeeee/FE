a99eaeaa437871cd6ff455212fcaa17e457536ba
patrick333c <jdmalaluan2@gmail.com>
2024-09-11 05:24:56 +0600

    new logo

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,10 @@
-import {
-    fileURLToPath,
-    URL
-} from 'node:url';
+import { fileURLToPath, URL } from 'node:url';
 
-import {
-    PrimeVueResolver
-} from '@primevue/auto-import-resolver';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
-import {
-    defineConfig
-} from 'vite';
-import {
-    VitePWA
-} from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,16 +22,17 @@ export default defineConfig({
                 name: 'Anahaw',
                 short_name: 'Anahaw',
                 description: 'Anahaw app',
-      start_url: "/auth/welcome",
-      background_color: "#289c3c",
-      theme_color: '#289c3c',
-                icons: [{
-                        src: 'pwa-192x192.png',
+                start_url: '/auth/welcome',
+                background_color: '#289c3c',
+                theme_color: '#289c3c',
+                icons: [
+                    {
+                        src: 'logo.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: 'Sharkcuet.png',
+                        src: 'logo-big.png',
                         sizes: '512x512',
                         type: 'image/png'
                     },
@@ -59,8 +51,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src',
-                import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
 });
